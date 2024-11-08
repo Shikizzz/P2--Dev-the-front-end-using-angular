6aa0d3b939ed4b479215841f0258d7633f4e8607
Shikizzz <widemann.simon@outlook.fr>
2024-11-08 10:27:58 +0100

    Created models and adapted service

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,24 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
+import { WinPerCountry } from '../dto/winPerContry';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympic: Olympic[] = {} as Olympic[];  //for initialization of the following BehaviorSubject
+  private olympics$ = new BehaviorSubject<Olympic[]>(this.olympic);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        this.olympics$.next(this.olympic);
         return caught;
       })
     );
@@ -27,5 +30,17 @@ export class OlympicService {
 
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+  getPieChartData() {
+    let pieChartData: WinPerCountry[];
+    let medalsCounter: number = 0;
+    this.olympics$.subscribe((olympics) => {
+      olympics.forEach((olympic) => {
+        olympic.participations.forEach((participation) => medalsCounter += participation.medalsCount);
+        pieChartData.push(new WinPerCountry(olympic.country, medalsCounter));
+        medalsCounter = 0;
+      })
+    });
+    return WinPerCountry;
   }
 }
